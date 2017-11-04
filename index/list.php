0401758c2836addf541479a09bc2b104a6f9d0c4
hongda <1020864884@qq.com>
2017-11-04 19:48:20 +0800

    'data'

---
+++ index/list.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @Author: 宏达
+ * @Date:   2017-11-03 16:58:44
+ * @Last Modified by:   宏达
+ * @Last Modified time: 2017-11-04 10:51:08
+ */
+header('Content-type:text/html; charset=utf8');
+include 'header.php';
+include 'ban.php';
+$cid=$_GET['cid'];
+$sql="select template,cname from category where cid={$cid}";
+$data=$mysql->query($sql)->fetch_assoc();
+$template=$data['template'];
+$cname=$data['cname'];
+$sql="select * from article where cid={$cid}";
+$list=$mysql->query($sql)->fetch_all(MYSQLI_ASSOC);
+// exit;
+include '../template/index/'.$template;
+include 'footer.php';
